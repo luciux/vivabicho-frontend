6d4f836c5483956faf8b74f95bbcba877d6905a6
Luciano Jr. <l_u_cio@hotmail.com>
2024-04-22 22:23:15 -0300

    Ajustado imports

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,16 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-import { Button } from 'primereact/button';
-import { Chart } from 'primereact/chart';
-import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
-import { Menu } from 'primereact/menu';
+
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../demo/service/ProductService';
-import { LayoutContext } from '../../layout/context/layoutcontext';
-import Link from 'next/link';
-import { Demo } from '@/types';
-import { ChartData, ChartOptions } from 'chart.js';
+import { ChartData } from 'chart.js';
 
 const lineData: ChartData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
